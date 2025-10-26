dcb868ba330c6ee0c8da33a7c47c8c32cf20bdd2
smabari <mdazizulbarishafin@gmail.com>
2025-10-26 14:46:30 +0600

    16_main_function

---
+++ python/1_pythonBasics/16_main_function/area.py
@@ -0,0 +1,6 @@
+def calcuilate_traingle_area(base, height):
+    print("value of __name__ : ", __name__)
+    return (base * height) / 2
+
+if __name__ == "__main__i":
+    print("Area: ", calcuilate_traingle_area(10, 5))
---
+++ python/1_pythonBasics/16_main_function/caller.py
@@ -0,0 +1,4 @@
+import area
+
+print("Inside caller.py")
+print(area.calcuilate_traingle_area(10, 30))
